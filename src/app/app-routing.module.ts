9ccf4657744205e77aab002c289a487860cd23ba
mrisho <mrisho.msabaha@gmail.com>
2018-06-16 22:04:04 -0500

    adding routes to the app.module and route to jobs for a specific job

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
+import { MyApplicationsComponent } from './jobs/my-applications/my-applications.component';
+import { ReviewApplicationsComponent } from './jobs/review-applications/review-applications.component';
+import { ViewJobsComponent } from './jobs/view-jobs/view-jobs.component';
+import { HomeComponent } from './home.component';
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ViewJobComponent } from './jobs/view-job/view-job.component';
 
 
 const routes: Routes = [
-
-
+ { path: "", component: HomeComponent},
+ { path: "my-applications", component: MyApplicationsComponent},
+ { path: "view-job/:id", component: ViewJobComponent, loadChildren: './jobs/jobs.module#JobsModule' },
+ { path: "view-jobs", component: ViewJobsComponent},
 ];
 
 @NgModule({
--- src/app/jobs/jobs.module.ts
+++ src/app/jobs/jobs.module.ts
@@ -4,11 +4,19 @@ import { PostJobsComponent } from './post-jobs/post-jobs.component';
 import { ViewJobsComponent } from './view-jobs/view-jobs.component';
 import { ReviewApplicationsComponent } from './review-applications/review-applications.component';
 import { JobRatingComponent } from './job-rating/job-rating.component';
+import { MyApplicationsComponent } from './my-applications/my-applications.component';
+import { ViewJobComponent } from './view-job/view-job.component';
+
+
+export const jobRoutes = [
+  { path: "rate-job", component: JobRatingComponent },
+  { path: "review-applications", component: ReviewApplicationsComponent}
+]
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [PostJobsComponent, ViewJobsComponent, ReviewApplicationsComponent, JobRatingComponent]
+  declarations: [PostJobsComponent, ViewJobsComponent, ReviewApplicationsComponent, JobRatingComponent, MyApplicationsComponent, ViewJobComponent]
 })
 export class JobsModule { }
