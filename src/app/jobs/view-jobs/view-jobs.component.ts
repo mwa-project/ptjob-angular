435b0a6a1c2416d5f8bbabd63a750eb857969c46
mrisho <mrisho.msabaha@gmail.com>
2018-06-18 18:33:38 -0500

    job list table from angular material complete

--- src/app/jobs/view-jobs/view-jobs.component.ts
+++ src/app/jobs/view-jobs/view-jobs.component.ts
@@ -13,40 +13,45 @@ import { JobService } from '../../services/job.service';
 export class ViewJobsComponent implements OnInit {
 
 
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns = ['index','category', 'description', 'start_date', 'end_date', 'view'];
+  dataSource ;
   
   // constructor() { }
 
-  jobs: Array<Object>;
-  constructor(private jobService: JobService) { }
+  //jobs: Array<Object>;
+  constructor(private jobService: JobService) {
+
+    this.jobService.getJobs().subscribe(list => {
+      this.dataSource = list;
+    });
+   }
 
 
   ngOnInit() {
-    this.jobService.getJobs().subscribe(list => {
-      this.jobs = list;
-    });
+    // this.jobService.getJobs().subscribe(list => {
+    //   this.jobs = list;
+    // });
   }
 
 }
 
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
+// export interface Job {
+//   name: string;
+//   position: number;
+//   weight: number;
+//   symbol: string;
+// }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+// const ELEMENT_DATA: Job[] = [
+//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+//   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+// ];
