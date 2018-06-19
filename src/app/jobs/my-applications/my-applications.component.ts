9504261d21f99bc70443150a2652f366627f1497
backslash112 <yangcun@live.com>
2018-06-18 22:16:10 -0500

    show applications list finished.

--- src/app/jobs/my-applications/my-applications.component.ts
+++ src/app/jobs/my-applications/my-applications.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../users/user.service';
+import { ActivatedRoute } from '@angular/router';
+import { ApplicationService } from '../../services/application.service';
 
 @Component({
   selector: 'app-my-applications',
@@ -11,40 +13,57 @@ export class MyApplicationsComponent implements OnInit {
   user: Object;
   apps: Array<Object>;
   isAccept = true;
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private activatedRoute: ActivatedRoute, private appliationService: ApplicationService) { }
 
   ngOnInit() {
-    this.user = this.userService.getCurrentUser();
-    this.apps = this.user['job_applications'];
-    this.apps = [{
-      job_id: '5b27da26eab89f87b1897cc7',
-      job_name: "Sales Associate job" , 
-      posted_date: Date.now(), 
-      applied_date: Date.now(), 
-      status: "Done", 
-      start_date: Date.now(), 
-      end_date: Date.now()
-    },
-    {
-      job_id: '5b27da26eab89f87b1897cc7',
-      job_name: "Package Handler " , 
-      posted_date: Date.now(), 
-      applied_date: Date.now(), 
-      status: "Accept", 
-      start_date: Date.now(), 
-      end_date: Date.now()
-    },
-    {
-      job_id: '5b27da26eab89f87b1897cc7',
-      job_name: "Package Handler " , 
-      posted_date: Date.now(), 
-      applied_date: Date.now(), 
-      status: "Pending", 
-      start_date: Date.now(), 
-      end_date: Date.now()
-    }];
-    console.log(this.user);
-    console.log(this.apps);
+    this.activatedRoute.params.subscribe(params => {
+      let jobId = params['jobId'];
+      if (jobId) {
+        console.log('get applications by job ID');
+        this.appliationService.getApplicationsByJobId(jobId, apps => {
+          this.apps = apps;
+          console.log(this.apps);
+        });
+      } else {
+        console.log('get applications by token (my applications)');
+        this.appliationService.getMyApplications(apps => {
+          this.apps = apps;
+          console.log(this.apps);
+        });
+      }
+    });
+    
+    // this.user = this.userService.getCurrentUser();
+    // this.apps = this.user['job_applications'];
+    // this.apps = [{
+    //   job_id: '5b27da26eab89f87b1897cc7',
+    //   job_name: "Sales Associate job" , 
+    //   posted_date: Date.now(), 
+    //   applied_date: Date.now(), 
+    //   status: "Done", 
+    //   start_date: Date.now(), 
+    //   end_date: Date.now()
+    // },
+    // {
+    //   job_id: '5b27da26eab89f87b1897cc7',
+    //   job_name: "Package Handler " , 
+    //   posted_date: Date.now(), 
+    //   applied_date: Date.now(), 
+    //   status: "Accept", 
+    //   start_date: Date.now(), 
+    //   end_date: Date.now()
+    // },
+    // {
+    //   job_id: '5b27da26eab89f87b1897cc7',
+    //   job_name: "Package Handler " , 
+    //   posted_date: Date.now(), 
+    //   applied_date: Date.now(), 
+    //   status: "Pending", 
+    //   start_date: Date.now(), 
+    //   end_date: Date.now()
+    // }];
+    // console.log(this.user);
+    // console.log(this.apps);
   }
 
 }
---
+++ src/app/services/application.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { UserService } from '../users/user.service';
+import { server } from '../../config';
+import { JobService } from './job.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApplicationService {
+
+  constructor(private userService: UserService, private jobService: JobService) { }
+
+
+  private getUrl(params?: string): string {
+    let url = server.host + ":" + server.port + '/applications';
+    if (params) {
+      url += ('/' + params);
+    }
+    return url;
+  }
+  
+  public getMyApplications(callback) {
+    callback(this.userService.getCurrentUser()['job_applications']);
+  }
+
+  public getApplicationsByJobId(jobId: string, callback) {
+    this.jobService.getJobById(jobId).subscribe(res => {
+      console.log(res)
+      callback(res['data']['applicants']);
+    });
+  }
+}
